1ce2698dd8bf34cf07bdc0613f534b279d74cb0a
JuanDavinLie <juandavinlie88@gmail.com>
2020-10-21 13:56:43 +0800

    Added tut group list panel

--- src/main/java/seedu/address/ui/TutorialGroupListPanel.java
+++ src/main/java/seedu/address/ui/TutorialGroupListPanel.java
@@ -1,4 +1,51 @@
 package seedu.address.ui;
 
-public class TutorialGroupListPanel {
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.TutorialGroup;
+import seedu.address.ui.TutorialGroupCard;
+import seedu.address.ui.UiPart;
+
+import java.util.logging.Logger;
+
+/**
+ * Panel containing the list of persons.
+ */
+public class TutorialGroupListPanel extends UiPart<Region> {
+    private static final String FXML = "TutorialGroupListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(TutorialGroupListPanel.class);
+
+    @javafx.fxml.FXML
+    private ListView<TutorialGroup> tutorialGroupListView;
+
+    /**
+     * Creates a {@code TutorialGroupListPanel} with the given {@code ObservableList}.
+     */
+    public TutorialGroupListPanel(ObservableList<TutorialGroup> tutorialGroupList) {
+        super(FXML);
+        tutorialGroupListView.setItems(tutorialGroupList);
+        tutorialGroupListView.setCellFactory(listView -> new TutorialGroupListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code TutorialGroup} using a {@code TutorialGroupCard}.
+     */
+    class TutorialGroupListViewCell extends ListCell<TutorialGroup> {
+        @Override
+        protected void updateItem(TutorialGroup tutorialGroup, boolean empty) {
+            super.updateItem(tutorialGroup, empty);
+
+            if (empty || tutorialGroup == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new TutorialGroupCard(tutorialGroup, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
 }
